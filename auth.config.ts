96aa3da516ba6d676abbc522c13a7c06942805c5
DESKTOP-610QLQU\Đình Tiến <tiennd35@fpt.com>
2025-10-08 19:30:03 +0700

    update auth config to login with google

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
-import type { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
@@ -8,22 +9,6 @@ export const authConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  pages: {
-    signIn: "/",
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub!;
-      }
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.sub = user.id;
-      }
-      return token;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-} satisfies NextAuthOptions;
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
